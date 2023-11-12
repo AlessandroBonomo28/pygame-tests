defc1b552cf935ef0e64c277a5b068a6d629c7a0
Alexbonny28 <al.bonomo@studenti.unina.it>
2023-11-12 17:39:43 +0100

    ww II aircraft menu

---
+++ planes/classes/button.py
@@ -0,0 +1,78 @@
+import pygame
+class MyButton:
+    text : str
+    position : tuple
+    width : int
+    height : int
+    bg_color : tuple
+    text_color : tuple
+    font : pygame.font
+    __blink : bool
+    __timer_blink : float
+    __timer_blink_max : float = 500
+    __timer_blink_color : tuple 
+    __text_blink_color : tuple
+    __original_text_color : tuple
+    __original_color : tuple
+    __border_stroke : int 
+    __border_color : tuple 
+    def __init__(self, text, position_center, bg_color, text_color, font,blink = False, border_stroke = 2, border_color = (0,0,0), timer_blink_color = (0,255,0), text_blink_color = (0,0,0)):
+        self.text = text
+        self.position = position_center
+        self.width = font.size(text)[0] + 20
+        self.height = font.get_height() + 5
+        self.__timer_blink_color = timer_blink_color
+        self.__text_blink_color = text_blink_color
+        self.__border_stroke = border_stroke
+        self.__border_color = border_color
+        self.bg_color = bg_color
+        self.font = font
+        self.text_color = text_color
+        self.__original_text_color = text_color
+        self.__blink = blink
+        self.__original_color = bg_color
+        self.__timer_blink = 0
+
+    def set_new_color(self, color):
+        self.__original_color = color
+
+    def draw(self, screen):
+        if self.__blink and self.__timer_blink < self.__timer_blink_max:
+            self.__timer_blink += pygame.time.Clock().tick(60)
+        elif self.__blink and self.__timer_blink >= self.__timer_blink_max:
+            self.__timer_blink = 0
+            if self.bg_color == self.__timer_blink_color:
+                self.set_original_color()
+            else:
+                self.set_blink_color()
+        else:
+            self.bg_color = self.__original_color
+        pygame.draw.rect(screen, self.bg_color, pygame.Rect(self.position[0] - self.width // 2, self.position[1] - self.height // 2, self.width, self.height))
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect()
+        text_rect.center = self.position
+        screen.blit(text_surface, text_rect)
+        self.__draw_border(screen)
+
+    def __draw_border(self, screen):
+        pygame.draw.rect(screen, self.__border_color, pygame.Rect(self.position[0] - self.width // 2, self.position[1] - self.height // 2, self.width, self.height), self.__border_stroke)
+
+    def set_blink_color(self):
+        self.bg_color = self.__timer_blink_color
+        self.text_color = self.__text_blink_color
+
+    def set_original_color(self):
+        self.bg_color = self.__original_color
+        self.text_color = self.__original_text_color
+
+    def set_blink(self, blink):
+        self.__blink = blink
+    
+    def is_blinking(self):
+        return self.__blink
+    
+    def mouse_over(self,mouse_position) -> bool:
+        return self.position[0] - self.width // 2 <= mouse_position[0] <= self.position[0] + self.width // 2 and self.position[1] - self.height // 2 <= mouse_position[1] <= self.position[1] + self.height // 2
+    
+        
+
--- planes/game.py
+++ planes/game.py
@@ -1,9 +1,10 @@
 
 import pygame,datetime,json
-import logging
+import logging, math,random
 from tkinter import *
 from tkinter import messagebox
 from classes.logger import Logger
+from classes.button import MyButton
 logging.basicConfig(level = logging.DEBUG)
 
 # force single instance
@@ -24,6 +25,7 @@ except:
 	year_born = 1934
 
 clock = pygame.time.Clock()
+FPS = 60
 boot_time = datetime.datetime.now()
 
 logger = Logger()
@@ -55,9 +57,10 @@ boom_sound = pygame.mixer.Sound("data/sounds/boom.mp3")
 pygame.mixer.music.load('data/music/loop.mp3')
 pygame.mixer.music.play(-1)
 
-font_size = 20
-game_font = pygame.font.SysFont('Comic Sans MS', font_size)
-
+default_font_size = 20
+font_size_btn = 40
+default_font = pygame.font.SysFont('Comic Sans MS', default_font_size)
+btn_font = pygame.font.SysFont('Comic Sans MS', font_size_btn)
 
 
 
@@ -89,11 +92,67 @@ if now.day == day_born and now.month == month_born:
 	popUp("Oggi, "+str_now+" è un giorno speciale!",f"Buon {years}° compleanno {player_name}!")
 	pygame.mixer.music.unpause()
 
-while not exit:	
+
+btn_start = MyButton("START", position_center=(width // 2, height // 2), 
+					 bg_color=(0,0,0), text_color=(255,255,255), font=btn_font,border_stroke=2,
+					 border_color=(255,255,255))
+
+scroll = 0
+
+
+bg_images = []
+for i in range(1, 5):
+	bg_image = pygame.image.load(f"data/images/parallax/{i}.png").convert_alpha()
+	bg_images.append(bg_image)
+bg_images = [pygame.transform.scale(i, (width, height)) for i in bg_images]
+bg_width = bg_images[0].get_width()
+# scale image to fit screen
+
+
+def draw_bg():
+	global scroll
 	
-	canvas.fill((0,0,0))
+	x = 0
+	speed = 1
+	for i in bg_images:
+		xdraw = (x * bg_width) - scroll * speed
+		canvas.blit(i, (xdraw, 0))
+		speed += 0
+
+		canvas.blit(i, (bg_width+ xdraw, 0))
 
 	
+
+		
+plane_img = pygame.image.load("data/images/planes/GER_bf109.png")		
+px = 0
+py = 0
+scale = width // 6
+plane_img = pygame.transform.scale(plane_img, (scale,scale))
+# rotate 90
+
+plane_img = pygame.transform.rotate(plane_img, -90)
+while not exit:	
+
+	clock.tick(FPS)
+
+	#draw world
+	draw_bg()
+
+	scroll += 2
+	scroll %= bg_width
+	
+
+	#canvas.fill((0,0,0))
+	btn_start.draw(canvas)
+	mouse_pos = pygame.mouse.get_pos()
+	sign = -1 if pygame.time.get_ticks() % 5000 < 2000 else 1
+	dy =   mouse_pos[1] - py
+	dx =   mouse_pos[0] - px
+	px += dx//65  -  ((dx/120) * math.sin(pygame.time.get_ticks()*0.003)) 
+	py += dy/65  +  ((dy/40) * math.cos(pygame.time.get_ticks()*0.006)) - sign*math.cos(pygame.time.get_ticks()*0.013)
+
+	canvas.blit(plane_img, (px, py))
 	for event in pygame.event.get():
 		# if resized window
 		if event.type == pygame.VIDEORESIZE:
@@ -111,8 +170,6 @@ while not exit:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
 				exit = True
-			if event.key == pygame.K_s:
-				showing_cards = not showing_cards
 			if event.key == pygame.K_m:
 				audio_enabled = not audio_enabled
 				if audio_enabled:
@@ -120,7 +177,10 @@ while not exit:
 				else:
 					pygame.mixer.music.pause()
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			logging.debug("resetting game")
+			#get mouse position
+			pos = pygame.mouse.get_pos()
+			if btn_start.mouse_over(pos):
+				logging.debug("start")
 			break
 		
 			
